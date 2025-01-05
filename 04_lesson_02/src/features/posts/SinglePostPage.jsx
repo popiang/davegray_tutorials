1bc4f39ac959babf2bf63a15df9457f43b21d01a
SHAHRIL MAD SHAH <popiang@hotmail.com>
2025-01-05 22:18:41 +0800

    asdf

---
+++ 04_lesson_02/src/features/posts/SinglePostPage.jsx
@@ -0,0 +1,34 @@
+import { useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { selectPostById } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionsButtons from "./ReactionsButtons";
+
+const SinglePostPage = () => {
+    const { postId } = useParams();
+    const post = useSelector((state) => selectPostById(state, Number(postId)));
+
+    if (!post) {
+        return (
+            <section>
+                <h2>Post is not found!</h2>
+            </section>
+        );
+    }
+
+    return (
+        <article className="single-post-article">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <p>
+				<Link to={`/post/edit/${postId}`}>Edit Post</Link>
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
+            <ReactionsButtons post={post} />
+        </article>
+    );
+};
+
+export default SinglePostPage;
